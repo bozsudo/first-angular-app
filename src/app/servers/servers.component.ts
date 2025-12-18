fbea957b70cb723296f4722307982eaf0a618bfc
Yunus Bozkurt <yunusbozkurt7000@gmail.com>
2025-12-18 10:55:28 +0000

    348 property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,11 +4,21 @@ import { Component } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
+  waitingForNewServer: boolean = true;
+
+  constructor() {
+    setTimeout(
+      () => this.waitingForNewServer = false,
+      5000
+    )
+  }
 
 }
