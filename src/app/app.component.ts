91fafba0b7ebc56667be615ca0589f0b7f4b5735
Yunus Bozkurt <yunusbozkurt7000@gmail.com>
2025-12-19 10:21:30 +0000

    Assignment 3: Practicing Directives 1. Add a button which says "Display Details". 2. Add a paragraph with any content of your choice (e.g. 'Secret Password = tuna'). 3. Toggle the displaying of that paragraph with the button created in the first step. 4. Log all button clicks in an array and output that array below the secret paragraph (maybe log a timestamp or simply an incrementing number). 5. Starting at the 5th log item, give all future log items a blue background (via ngStyle) and white color (ngClass).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,18 @@ import { Component } from '@angular/core';
     h3 {
       color: dodgerblue;
     }
+    .text-white {
+      color: white;
+    }
   `]
 })
 export class AppComponent {
+  displayState: boolean = false;
+  clickCount: number = 0;
+  logEvents: Array<[number, Date]> = [];
+
+  onDisplay(event: Event) {
+    this.displayState = !this.displayState;
+    this.logEvents.push([++this.clickCount, new Date()]);
+  }
 }
