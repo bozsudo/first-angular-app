678b025bf7572453ce771a21c1ac802278eb0205
Yunus Bozkurt <yunusbozkurt7000@gmail.com>
2025-12-20 14:41:34 +0000

    371 setting up the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [`
-    h3 {
-      color: dodgerblue;
-    }
-    .text-white {
-      color: white;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  displayState: boolean = false;
-  clickCount: number = 0;
-  logEvents: Array<[number, Date]> = [];
-
-  onDisplay(event: Event) {
-    this.displayState = !this.displayState;
-    this.logEvents.push([++this.clickCount, new Date()]);
-  }
-}
+export class AppComponent {}
