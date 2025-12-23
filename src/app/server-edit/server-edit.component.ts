7f7329fa5b2a9b84c3dbf28a3afe05eb2cf4514f
Yunus Bozkurt <yunusbozkurt7000@gmail.com>
2025-12-23 10:18:13 +0000

    397 using local references in templates

--- src/app/server-edit/server-edit.component.ts
+++ src/app/server-edit/server-edit.component.ts
@@ -8,14 +8,14 @@ import {Component, EventEmitter, Output} from '@angular/core';
 export class ServerEditComponent {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName: string = '';
+  // newServerName: string = '';
   newServerContent: string = '';
 
-  onAddServer() {
-    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddServer(serverName: HTMLInputElement) {
+    this.serverCreated.emit({serverName: serverName.value, serverContent: this.newServerContent});
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddBlueprint(serverName: HTMLInputElement) {
+    this.blueprintCreated.emit({serverName: serverName.value, serverContent: this.newServerContent});
   }
 }
