4379511a4c13cd1506cb100c87c12f5b7709785e
Yunus Bozkurt <yunusbozkurt7000@gmail.com>
2025-12-23 10:25:32 +0000

    399 getting access to the template & DOM with ViewChild()

--- src/app/server-edit/server-edit.component.ts
+++ src/app/server-edit/server-edit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-server-edit',
@@ -9,13 +9,14 @@ export class ServerEditComponent {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   // newServerName: string = '';
-  newServerContent: string = '';
+  // newServerContent: string = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   onAddServer(serverName: HTMLInputElement) {
-    this.serverCreated.emit({serverName: serverName.value, serverContent: this.newServerContent});
+    this.serverCreated.emit({serverName: serverName.value, serverContent: this.serverContentInput.nativeElement.value});
   }
 
   onAddBlueprint(serverName: HTMLInputElement) {
-    this.blueprintCreated.emit({serverName: serverName.value, serverContent: this.newServerContent});
+    this.blueprintCreated.emit({serverName: serverName.value, serverContent: this.serverContentInput.nativeElement.value});
   }
 }
