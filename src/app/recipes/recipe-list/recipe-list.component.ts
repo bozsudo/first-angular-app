b7d916157ee34e64a0f095ed3190a0d141e8711b
Yunus Bozkurt <yunusbozkurt7000@gmail.com>
2026-01-05 07:15:19 +0000

    442 managing recipes in a recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,25 +1,21 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe } from "../recipe.model";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
-  recipes: Recipe[] = [
-    new Recipe(
-      'Carbonara',
-      'This classic Italian pasta dish combines a silky cheese sauce with crisp pancetta and black pepper.',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1001491_11-2e0fa5c.jpg'
-    ),
-    new Recipe(
-      'Spaghetti Bolognese',
-      'Spaghetti is a classic, long, thin, cylindrical Italian pasta made from durum wheat, water, and sometimes eggs.',
-      'https://cravinghomecooked.com/wp-content/uploads/2024/07/spaghetti-bolognese-1-22.jpg'
-    )
-  ];
+export class RecipeListComponent implements OnInit {
   @Output() onRecipeSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
+
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onSelect(recipe: Recipe) {
     this.onRecipeSelected.emit(recipe);
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Recipe} from "./recipe.model";
 
-@Injectable({
-  providedIn: 'root'
-})
 export class RecipeService {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Carbonara',
+      'This classic Italian pasta dish combines a silky cheese sauce with crisp pancetta and black pepper.',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1001491_11-2e0fa5c.jpg'
+    ),
+    new Recipe(
+      'Spaghetti Bolognese',
+      'Spaghetti is a classic, long, thin, cylindrical Italian pasta made from durum wheat, water, and sometimes eggs.',
+      'https://cravinghomecooked.com/wp-content/uploads/2024/07/spaghetti-bolognese-1-22.jpg'
+    )
+  ];
 
-  constructor() { }
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
